3110bf8f25b081f6dc5d737be509352877246342
GK-2211 <84940658+GK-2211@users.noreply.github.com>
2022-02-23 19:17:58 +0530

    second

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'home' , component : HomeComponent},
+  {path : 'about' , component : AboutComponent},
+  {path : 'contact' , component : ContactComponent},
+  {path : 'login', component : LoginComponent},
+  {path : 'signup' , component : SignupComponent}
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes , {anchorScrolling : 'enabled'} )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private viewportscroller : ViewportScroller) { }
+
+  ngOnInit(): void {
+  }
+  onClick(id : string){
+    this.viewportscroller.scrollToAnchor(id);
+  }
+}
