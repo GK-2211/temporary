c2eef08bf639c6f5524b932b03ae9f3f57d9fb07
GK-2211 <84940658+GK-2211@users.noreply.github.com>
2022-02-26 23:43:18 +0530

    edit 2

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ViewportScroller } from '@angular/common';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private viewportscroller : ViewportScroller) { }
 
   ngOnInit(): void {
   }
-
+  onClick(id : string){
+    this.viewportscroller.scrollToAnchor(id);
+  }
 }
